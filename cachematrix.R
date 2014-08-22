acbfcc67880cc1a4b2729648de65814b1f6aea46
asder <asder@asderbox.(none)>
2014-08-22 20:23:13 +0400

    caching for inverse matrix multiplication

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## tweaks to makeVector function taken from assignment:
+## replaced makeVector by makeCacheMatrix
+## replaced 'mean' by 'inverse'
+## warning! never ever do this in real life project
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        ## assignment takes place in global environment
+        x <<- y
+        m <- NULL
+    }
 
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    
+    ## functions hold pointer to defining environment
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## tweaks to cachemean taken from assignment
+## replaced 'mean' by 'inverse'
+## replaced solving function
+## warning! never do this in real life project
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinverse(m)
+    m
 }
